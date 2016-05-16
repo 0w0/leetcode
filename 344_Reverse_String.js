07f577198417738f54d553658a458aba6bf7d429
0w0 <anonymous.0w0@gmail.com>
2016-05-16 19:36:39 +0800

    init

---
+++ 344_Reverse_String.js
@@ -0,0 +1,18 @@
+/**
+ *  Runtime: 148 ms
+ *  Sorry. We do not have enough accepted submissions.
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function (s) {
+    var l = s.length;
+    var r_s = '';
+    for (var i = l - 1; i >= 0; i--) {
+        r_s += s.charAt(i);
+    }
+
+    return r_s;
+};
