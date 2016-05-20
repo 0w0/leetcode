b82d0f9bf1415ae49086bdf39a1b21544a788795
0w0 <anonymous.0w0@gmail.com>
2016-05-20 19:45:16 +0800

    100_Same_Tree

---
+++ 100_Same_Tree.js
@@ -0,0 +1,50 @@
+/**
+ * Runtime: 104 ms
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    return JSON.stringify(p) === JSON.stringify(q);
+};
+
+/**
+ * Runtime: 104 ms
+ */
+var isSameTree = function(p, q) {
+    // First element assertion, since {} always != {}
+    // Both null
+    if (!p && !q) {
+        return true;
+    }
+    // One is null
+    if (!p || !q) {
+        return false;
+    }
+
+    // Main assertion
+    if (p.val !== q.val) {
+        return false;
+    }
+
+    // Recursion
+    if ( ! isSameTree(p.left, q.left)) {
+        return false;
+    }
+
+    if ( ! isSameTree(p.right, q.right)) {
+        return false
+    }
+
+    return true;
+};
