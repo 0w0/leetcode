aad0514456985b1be26f98101aaede1f023b5367
0w0 <anonymous.0w0@gmail.com>
2016-07-16 11:31:34 +0800

    init c

--- 231_Power_of_Two.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isPowerOfTwo = function(n) {
-    return Number.isInteger(Math.log2(n));
-};
--- 326_Power_of_Three.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isPowerOfThree = function(n) {
-    let log3 = function() { return Math.log10(n) / Math.log10(3) };
-    
-    return Number.isInteger(log3(n))
-};
