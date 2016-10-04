10f4ea5b170f02129bc282b504b9f3777689b6ae
0w0 <anonymous.0w0@gmail.com>
2016-10-04 22:07:23 +0800

    fix syntax

--- 100_Same_Tree.c
+++ 100_Same_Tree.c
@@ -10,8 +10,8 @@ bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
   if (p == NULL && q == NULL) return true;
   if (p == NULL || q == NULL) return false;
 
-  if (p.val == q.val) {
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  if (p->val == q->val) {
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
   } else {
     return false;
   }
