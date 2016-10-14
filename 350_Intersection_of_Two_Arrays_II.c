25dde949a9763487b4d8345b91f24913a4acabb3
0w0 <anonymous.0w0@gmail.com>
2016-10-14 23:14:10 +0800

    350_Intersection_of_Two_Arrays_II

--- 350_Intersection_of_Two_Arrays_II.c
+++ 350_Intersection_of_Two_Arrays_II.c
@@ -1,21 +1,22 @@
-#include <stdlib.h>
-
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
   *returnSize = 0;
   int *ret = malloc(*returnSize * sizeof(int));
   int retIdx = 0;
   for (int i = 0; i < nums1Size; i++) {
+    int same_in_num1 = 0;
+    for (int l = 0; l < nums1Size; l++) {
+      if (nums1[i] == nums1[l]) same_in_num1++;
+    }
     int same_in_num2 = 0;
-    int same_in_ret = 0;
     for (int j = 0; j < nums2Size; j++) {
       if (nums1[i] == nums2[j]) {
         same_in_num2++;
+        int same_in_ret = 0;
         for (int k = 0; k < *returnSize; k++) {
-          if (nums1[i] == ret[k]) {
-            same_in_ret++;
-          }
+          if (nums1[i] == ret[k]) same_in_ret++;
         }
-        if (same_in_num2 <= same_in_ret) continue;
+        int at_least = same_in_num1 > same_in_num2 ? same_in_num2 : same_in_num1;
+        if (same_in_ret >= at_least) continue;
 
         // Realloc ret
         *returnSize = *returnSize + 1;
@@ -28,8 +29,4 @@ int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* return
   }
 
   return ret;
-}
-
-int main() {
-  return 0;
 }
