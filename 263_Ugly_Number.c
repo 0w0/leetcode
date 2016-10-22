8e35cc418178596d1433b7d39e2c11f9bc95c720
0w0 <anonymous.0w0@gmail.com>
2016-10-22 12:17:02 +0800

    263_Ugly_Number

---
+++ 263_Ugly_Number.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+bool isUgly(int num) {
+  while(num >=2) {
+    if (num % 2 == 0) num = num / 2;
+    else if (num % 3 == 0) num = num / 3;
+    else if (num % 5 == 0) num = num / 5;
+    else return false;
+  }
+  
+  if (num == 1) return true;
+
+  return false;
+}
+
+int main() {
+
+}
